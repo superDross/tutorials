72d99fb5635c73cd725eb34a5dc9b6e8171eea43
David Ross <david.ross@yougov.com>
2019-08-19 21:17:51 +0100

    Viewset style

--- django_tutorials/drf/snippets/urls.py
+++ django_tutorials/drf/snippets/urls.py
@@ -1,23 +1,17 @@
-from django.urls import include, path
-from rest_framework.urlpatterns import format_suffix_patterns
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-urlpatterns = [
-    path("", views.api_root),
-    path("api-auth/", include("rest_framework.urls")),
-    path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
-    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
-    # path("snippets/", views.snippet_list),
-    # path("snippets/<int:pk>/", views.snippet_detail),
-    path("users/", views.UserList.as_view(), name="user-list"),
-    path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
-    path(
-        "snippets/<int:pk>/highlight/",
-        views.SnippetHighlight.as_view(),
-        name="snippet-highlight",
-    ),
-]
+# Routers automatically determine how the urls for an application
+# should be mapped
 
-# allows you to use suffix on links e.g. /downloads/1.json
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
